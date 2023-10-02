73f7f4d8b606af854e507222687e50ad9b2ce0c1
Pablo Martínez López <pmartinezl@full-on-net.com>
2023-10-02 16:16:51 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pablo Martinez Lopez on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
